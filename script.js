589015cde82b575c4b63f19170c2e0571ba0b62f
Alexandre Hillesheim <design.infoxande@gmail.com>
2023-09-18 15:52:23 -0300

    Update 1.2.5

---
+++ script.js
@@ -0,0 +1,26 @@
+
+window.sr = ScrollReveal({reset:true});
+
+sr.reveal('.title-b1', {duration: 1000});
+
+sr.reveal('.description-b1', {duration: 1000});
+
+sr.reveal('.title-g1', {duration: 1000});
+
+sr.reveal('.description-g1', {duration: 1000});
+
+sr.reveal('.platforms-container', {duration: 1000});
+
+sr.reveal('.platforms', {duration: 1000});
+
+sr.reveal('.title-clients', {duration: 1000});
+
+sr.reveal('.description-clients', {duration: 1000});
+
+sr.reveal('.material-symbols-outlined', {duration: 1000});
+
+sr.reveal('.logo-titulo', {duration: 1000});
+
+sr.reveal('.clientes', {duration: 1000});
+
+sr.reveal('.logo-plataforma-clientes', {duration: 1000});
